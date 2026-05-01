d322130479af01838b9cfc005a4b83cca82bb757
rrassya19-bit <rrassys19@gmail.com>
2026-05-01 15:48:27 +0700

    Tambah logika penghapusan node di awal tengah dan akhir list

--- DoubleLingkeList.cpp
+++ DoubleLingkeList.cpp
@@ -104,5 +104,26 @@ public:
             cout << "Record not found" << endl;
             return;
         }
+
+        // Step 2: if node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // Step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // Step 2b: START.prev = NULL
+        }
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollno << " deleted" << endl;
     }
 };
