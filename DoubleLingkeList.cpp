dd68a29d6842958b71eda8f1bb3f158f0aa1713b
rrassya19-bit <rrassys19@gmail.com>
2026-05-01 15:51:33 +0700

    Tambah switch-case untuk menghubungkan menu dengan fungsi yang sesuai

--- DoubleLingkeList.cpp
+++ DoubleLingkeList.cpp
@@ -229,5 +229,28 @@ int main()
 
         cin >> choice;
 
+        switch (choice)
+        {
+        case '1':
+            list.addnode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "\nInvalid option" << endl;
+        }
+
     } while (choice != '6');
 }
